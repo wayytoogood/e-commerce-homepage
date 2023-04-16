4f6bd19f91f323b681eff789ff47f12e0f64a28b
wayytoogood <huseyinsuna1903@gmail.com>
2023-04-17 01:48:33 +0300

    slider functionality fixed both mobile and desktop

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,66 @@
 import './styles/main.scss'
 
-const slider = document.querySelector(
+const slider = document.querySelector('.scroll-container') as HTMLElement
+const productContainer = document.querySelector(
   '.product-container__products'
 ) as HTMLElement
-let isDown = false
-let startX = 0
-let scrollLeft = 0
+const product = document.querySelector(
+  '.product-container__product'
+) as HTMLElement
+const prevBtn = document.querySelector('.nav-btn.prev') as HTMLButtonElement
+const nextBtn = document.querySelector('.nav-btn.next') as HTMLButtonElement
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true
-  slider.classList.add('active')
-  startX = e.pageX - slider.offsetLeft
-  scrollLeft = slider.scrollLeft
+// Slide movement with previous and next buttons
+let sliderWidth = slider.getBoundingClientRect().width
+let productContainerWidth = productContainer.getBoundingClientRect().width
+let productWidth = product.getBoundingClientRect().width
+
+let unvisibleArea = productContainerWidth - sliderWidth
+let totalMove = Math.ceil(unvisibleArea / productWidth)
+
+let oneMove = unvisibleArea / totalMove
+
+let currIndex = 0
+
+const move = () => {
+  prevBtn.style.display = 'grid'
+  nextBtn.style.display = 'grid'
+
+  if (currIndex === 0) {
+    prevBtn.style.display = 'none'
+  }
+  if (currIndex === totalMove) {
+    nextBtn.style.display = 'none'
+  }
+
+  productContainer.style.transform = `translateX(-${oneMove * currIndex}px)`
+}
+
+// For resetting slide states on resizing
+window.addEventListener('resize', () => {
+  sliderWidth = slider.getBoundingClientRect().width
+  productContainerWidth = productContainer.getBoundingClientRect().width
+  productWidth = product.getBoundingClientRect().width
+
+  unvisibleArea = productContainerWidth - sliderWidth
+  totalMove = Math.ceil(unvisibleArea / productWidth)
+
+  oneMove = unvisibleArea / totalMove
+
+  currIndex = 0
+  move()
 })
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false
-  slider.classList.remove('active')
+prevBtn.addEventListener('click', () => {
+  if (currIndex > 0) {
+    currIndex--
+    move()
+  }
 })
 
-slider.addEventListener('mouseup', () => {
-  isDown = false
-  slider.classList.remove('active')
-})
-
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return
-  e.preventDefault()
-  const x = e.pageX - slider.offsetLeft
-  const walk = (x - startX) * 1 // scroll speed
-  slider.scrollLeft = scrollLeft - walk
-  console.log(walk)
+nextBtn.addEventListener('click', () => {
+  if (currIndex < totalMove) {
+    currIndex++
+    move()
+  }
 })
